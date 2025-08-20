9aaabed2254cfcf8ed407fd2ed329767ac399d88
FelipeRPC-PROGRAMADOR <feliperpc900@outlook.com>
2025-08-20 02:11:25 +0000

    novas alterações

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 
 int main() {
-    printf ("CARTAS SUPER TRUNFO!\n");
+
+    char estado 
+    char codigo [10];
+    char cidade_cidade [100];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+    printf(" Digite o estado: \n");
+    scanf("%s", &estado);
+
+    printf(" Digite o codigo da carta: \n");
+    scanf("%s", &codigo);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &cidade);
+
+    printf("Digita a população: \n");
+    scanf("%d", &populacao);
+
+    printf ("Digite a area: \n");
+    scanf("%f", &area);
+
+    printf("Digite o pib: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o ponto turistico: \n");
+    scanf("%d", &pontos turisticos);
+
+
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
 }
 
 
